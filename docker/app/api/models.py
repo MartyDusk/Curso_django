09ac64a0772a6cabcd42d6cbb4f12090fde73ada
MartyDusk <jiracc36@outlook.com>
2026-07-23 13:08:57 -0600

    Minor fixes

--- docker/app/api/models.py
+++ docker/app/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Sucursal(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nombre
